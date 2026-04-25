8f45e93c9e0fdde6826352a70eeb53a2f487f792
Daniel <danielcaldasmonte@gmail.com>
2026-04-24 23:01:34 -0300

    Implement modern button-based Navbar and UI refinements

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import './Navigation.css';
 
 export default class Navigation extends React.Component {
   render() {
     return (
-      <ul className="nav nav-tabs" id="myTab" role="tablist">
-        <li className="nav-item">
-          <a className={"nav-link " + (this.props.selectedTab === 'home' ? 'active': '')} onClick={() => this.props.onClick("home")}
-            id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected={this.props.selectedTab === 'home'}>Home</a>
-        </li>
-        <li className="nav-item">
-          <a className={"nav-link " + (this.props.selectedTab === 'shipping-requests' ? 'active': '')} onClick={() => this.props.onClick("shipping-requests")}
-          id="shipping-requests-tab" data-toggle="tab" href="#shipping-requests" role="tab" aria-controls="shipping-requests" aria-selected={this.props.selectedTab === 'shipping-requests'}>Shipping Requests</a>
-        </li>
-      </ul>
+      <nav className="Navbar">
+        <div className="Navbar-brand">Shiptivitas</div>
+        <div className="Navbar-links">
+          <button 
+            className={"Nav-button " + (this.props.selectedTab === 'home' ? 'active': '')} 
+            onClick={() => this.props.onClick("home")}
+          >
+            Home
+          </button>
+          <button 
+            className={"Nav-button " + (this.props.selectedTab === 'shipping-requests' ? 'active': '')} 
+            onClick={() => this.props.onClick("shipping-requests")}
+          >
+            Shipping Requests
+          </button>
+        </div>
+      </nav>
     );
   }
 }
